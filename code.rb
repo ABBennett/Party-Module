187c0809d9531a9b93fcf3ab509aca84b757be6e
AlexBennett <alexanderbondbennett@gmail.com>
2015-11-11 17:19:23 -0500

    Worked with modules

---
+++ code.rb
@@ -0,0 +1,35 @@
+module Invitee
+  def invited?
+    true
+  end
+end
+
+module PartyGoer
+attr_reader :drink_count
+def initialize
+  @drink_count = 0
+end
+
+  def drink
+    if @drink_count >= 2
+      false
+    else
+      @drink_count += 1
+      true
+    end
+  end
+
+  def sing
+    "lala"
+  end
+
+  def cause_havoc
+    raise PersonalizedHavocError, "You should define this yourself!"
+  end
+
+  def self.invited?
+    true
+  end
+end
+
+class PersonalizedHavocError < StandardError; end
